fb6fe483e531e78d0366c903a533450dd3b64bdf
Karthik Raju <karthikraju391@gmail.com>
2022-11-07 23:36:38 +0530

    env update

--- api/server.js
+++ api/server.js
@@ -1,27 +1,27 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const cors = require('cors');
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
 dotenv.config();
 
 //routes
-const authRoute = require('./Routes/auth');
-const blogRoute = require('./Routes/blog');
+const authRoute = require("./Routes/auth");
+const blogRoute = require("./Routes/blog");
 
 app.use(cors());
 app.use(express.json());
 
 mongoose
-	.connect(`mongodb+srv://karthik:karthik@cluster0.q4p6ycd.mongodb.net/?retryWrites=true&w=majority`)
-	.then(() => console.log('Connected to DB!'))
+	.connect(process.env.MONGO_URL)
+	.then(() => console.log("Connected to DB!"))
 	.catch((err) => {
 		console.error(err);
 	});
 
-app.use('/api/blogs', blogRoute);
-app.use('/api/auth', authRoute);
+app.use("/api/blogs", blogRoute);
+app.use("/api/auth", authRoute);
 
 app.listen(process.env.PORT || 4000, () => {
-	console.log('Server is running!');
+	console.log("Server is running!");
 });
